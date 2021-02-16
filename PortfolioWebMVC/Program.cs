e49cc4e71117ab57c2b7cc9eed2632c85072bed7
mrbrunotte <mrbrunotte@gmail.com>
2021-02-16 23:58:36 +0100

    Fix connection string

--- PortfolioWebMVC/Program.cs
+++ PortfolioWebMVC/Program.cs
@@ -21,10 +21,10 @@ namespace PortfolioWebMVC
             Host.CreateDefaultBuilder(args)
 .ConfigureAppConfiguration((context, config) =>
 {
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//config.AddAzureKeyVault(
+//keyVaultEndpoint,
+//new DefaultAzureCredential());
 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
